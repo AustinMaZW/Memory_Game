26c9139542fa777e6719ba5661e2bdfc193e0a08
TCK285 <cktang89@gmail.com>
2021-07-14 21:48:31 +0800

    push to ck main page

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -9,43 +9,43 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        Button startGameBtn = findViewById(R.id.start_game_btn);
-//
-//        startGameBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (startGameBtn != null){
-//                    Intent startGame = new Intent(MainActivity.this, selectImageActivity.class);
-//                    startActivity(startGame);
-//                }
-//            }
-//        });
-//
-//        Button creditBtn = findViewById(R.id.credits_btn);
-//        creditBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (creditBtn != null){
-//                    Intent viewCredits = new Intent(MainActivity.this, viewCreditsActivity.class);
-//                    startActivity(viewCredits);
-//                }
-//            }
-//        });
-//
-//        Button descriptionBtn = findViewById(R.id.description_btn);
-//        descriptionBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (descriptionBtn != null){
-//                    Intent viewDescription = new Intent(MainActivity.this, viewDescriptionActivity.class);
-//                    startActivity(viewDescription);
-//                }
-//
-//            }
-//        });
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startGameBtn = findViewById(R.id.start_game_btn);
+
+        startGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (startGameBtn != null){
+                    Intent startGame = new Intent(MainActivity.this, selectImageActivity.class);
+                    startActivity(startGame);
+                }
+            }
+        });
+
+        Button creditBtn = findViewById(R.id.credits_btn);
+        creditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (creditBtn != null){
+                    Intent viewCredits = new Intent(MainActivity.this, viewCreditsActivity.class);
+                    startActivity(viewCredits);
+                }
+            }
+        });
+
+        Button descriptionBtn = findViewById(R.id.description_btn);
+        descriptionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (descriptionBtn != null){
+                    Intent viewDescription = new Intent(MainActivity.this, viewDescriptionActivity.class);
+                    startActivity(viewDescription);
+                }
+
+            }
+        });
+    }
 }
