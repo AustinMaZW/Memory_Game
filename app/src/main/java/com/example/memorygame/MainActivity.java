f92b69534695eecd5aae41b559884ba3a01d2616
TCK285 <cktang89@gmail.com>
2021-07-14 20:44:02 +0800

    push to ck main page

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.memorygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button startGameBtn = findViewById(R.id.start_game_btn);
-        Button creditBtn = findViewById(R.id.credits);
-        Button descriptionBtn = findViewById(R.id.description);
+//        Button startGameBtn = findViewById(R.id.start_game_btn);
+//
+//        startGameBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (startGameBtn != null){
+//                    Intent startGame = new Intent(MainActivity.this, selectImageActivity.class);
+//                    startActivity(startGame);
+//                }
+//            }
+//        });
 
+        Button creditBtn = findViewById(R.id.credits_btn);
+        creditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (creditBtn != null){
+                    Intent viewCredits = new Intent(MainActivity.this, viewCreditsActivity.class);
+                    startActivity(viewCredits);
+                }
+            }
+        });
+
+        Button descriptionBtn = findViewById(R.id.description_btn);
+        descriptionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (descriptionBtn != null){
+                    Intent viewDescription = new Intent(MainActivity.this, viewDescriptionActivity.class);
+                    startActivity(viewDescription);
+                }
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/memorygame/viewCreditsActivity.java
@@ -0,0 +1,14 @@
+package com.example.memorygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewCreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_credits);
+    }
+}
